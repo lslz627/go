827d6ab61de536a1b791aec418b046089c904b2b
lslz627 <lslz627@gmail.com>
2020-12-22 21:19:08 +0800

    删除无效代码

--- exercise/go.mod
+++
@@ -1,5 +0,0 @@
-module exercise
-
-go 1.14
-
-require github.com/fengyoulin/hookingo v0.1.1
--- exercise/int.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "reflect"
-
-func main() {
-	i := 0
-	t := reflect.TypeOf(i)
-	println(t)
-}
